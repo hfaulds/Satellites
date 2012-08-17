1977fe96e8d2666d21cb2530eacf4221da326313
Hayden Faulds <fauldsh@gmail.com>
2012-08-17 02:35:21 +0100

    Normals Fixed

--- src/render/Renderer2D.java
+++ src/render/Renderer2D.java
@@ -1,20 +1,14 @@
 package render;
 
-
-
 import javax.media.opengl.GL2;
 
 import scene.ui.Sprite;
-
-
 
 public class Renderer2D {
   public void preRender(GL2 gl, double width, double height) {
     gl.glMatrixMode(GL2.GL_PROJECTION);
     gl.glLoadIdentity();
     gl.glOrtho(0, width, height, 0, 0, 1);
-    gl.glDisable(GL2.GL_DEPTH_TEST);
-    gl.glDisable(GL2.GL_CULL_FACE);
     gl.glMatrixMode(GL2.GL_MODELVIEW);
     gl.glLoadIdentity();
     gl.glDisable(GL2.GL_LIGHTING);
